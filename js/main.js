f2bb233bf4bb5e4a62e221b6a693d9e5aa151999
Akash Sharma <akash.ibm121@gmail.com>
2024-09-12 13:43:30 +0530

    scroll fixed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-html
+//scroll btn
+
+const scrollBtn = document.getElementById("scroll-btn");
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
